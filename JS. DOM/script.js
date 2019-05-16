4be7c613189edda48bff95fa0daea5cc699d0c59
Yulia <46032626+YuliaBilchinskaya@users.noreply.github.com>
2019-05-16 15:24:38 +0300

    Update script.js

--- JS. DOM/script.js
+++ JS. DOM/script.js
@@ -1,4 +1,5 @@
-//1, 2
+//1. Дан элемент #elem. Добавьте ему класс "www".
+//2. Дан элемент #elem. Проверьте наличие у него класса "www", если есть - удалите класс.
 
  let classList = document.getElementById("elem").classList;
  	classList.add("www");
@@ -7,14 +8,16 @@
 }
 	console.log(document);
 
-//3
+//3. Напишите функцию поиска recursiveSearchTags, которая принимает на вход document и имя тега, 
+//а возвращает массив из всех элементов соответствующих этому тегу. Поиск идет в теге body.
 
 function recursiveSearchTags(document, d) {
     return document.querySelectorAll("#elem");
 }
 console.log(recursiveSearchTags(document.body, "div"));
 
-//4
+//4. Дан ul. Дан массив. Вставьте элементы этого массива в конец ul так, чтобы каждый элемент стоял в своем li.
+// Сделайте так, чтобы четные позиции имели красный фон.
 
 let arrLi = [1, 2, 3, 4, 5, 6, 7];
 let ul = document.querySelector("ul");
@@ -28,13 +31,13 @@ for (let i = 1; i <= arrLi.length; i++) {
 }
 console.log(ul);
 
-//5
+//5. Дан элемент #elem. Найдите его соседа сверху и добавьте ему в конец текст '!'
 
 let el = document.getElementById("elem").previousElementSibling.firstElementChild;
 el.innerHTML = el.innerHTML + "!";
 console.log(el);
 
-//6
+//6. Реализуйте функцию wrapInParagraph, которая находит текст (дочерние текстовые ноды) внутри элемента div и оборачивает текст в параграф
 
 function wrapInParagraph(el) {
   	let home = document.body.children[1].children[0];
@@ -44,7 +47,8 @@ function wrapInParagraph(el) {
 }
 wrapInParagraph();
 
-//7
+//7. Реализуйте функцию normalizeClassNames, которая нормализует имена классов для всех элементов на странице. 
+//В данном случае это означает что происходит преобразование всех классов написанных используя kebab нотацию в camelCase нотацию: text-center => textCenter
 
 function normalizeClassNames() {
     let str = document.querySelectorAll("*");
